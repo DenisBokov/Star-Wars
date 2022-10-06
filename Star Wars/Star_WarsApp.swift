60af30feabdcf6b9a8053d72286964ae654505f6
DenisBokov <57086505+DenisBokov@users.noreply.github.com>
2022-10-07 02:01:20 +0300

    Initial Commit

---
+++ Star Wars/Star_WarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Star_WarsApp.swift
+//  Star Wars
+//
+//  Created by Denis Bokov on 07.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct Star_WarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
